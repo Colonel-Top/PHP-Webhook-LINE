89000057a85584453f7e5478e420f884ce41f12f
promsurinm@hotmail.com <skr010527>
2017-04-27 21:39:48 +0700

    Fix index

--- index.php
+++ index.php
@@ -11,10 +11,11 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
+		// Reply only when message sent is fh;pin 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text']['groupId'];
+			echo $text;
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
